2d48d7d8b040d7bea1a4e1fcaba097eac2a47c5d
Nikita Samokhval <StoryOfTranquility@gmail.com>
2021-12-29 08:20:13 +0200

    Yep, hid API

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from "firebase/firestore"
 
 const config = {
-    apiKey: "AIzaSyB6Ya19qGtzhrAf6YyZUFrVnPj_-iEm0u4",
+    apiKey: process.env.VUE_APP_BACKEND_API_KEY,
     authDomain: "rick-backend.firebaseapp.com",
     databaseURL: "https://rick-backend-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "rick-backend",
     storageBucket: "rick-backend.appspot.com",
     messagingSenderId: "669470690290",
     appId: "1:669470690290:web:f3b64f5fecaed83f17796b"
-    // test
 };
 const firebaseApp = initializeApp(config)
 const firestore = getFirestore(firebaseApp)

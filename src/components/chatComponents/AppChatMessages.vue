b3d50e6416f01c9cb648639e3a47afa97c42fe10
Nikita Samokhval <StoryOfTranquility@gmail.com>
2022-01-15 01:00:54 +0200

    fix chat auto scroll and chat height

--- src/components/chatComponents/AppChatMessages.vue
+++ src/components/chatComponents/AppChatMessages.vue
@@ -1,16 +1,5 @@
 <template>
-  <div
-    class="
-      flex-1
-      p:2
-      sm:p-6
-      justify-between
-      flex flex-col
-      md:w-2/3
-      w-full
-      mx-auto
-    "
-  >
+  <div class="flex-1 p:2 sm:p-6 flex flex-col md:w-2/3 w-full mx-auto">
     <div v-if="messages" class="flex sm:items-center justify-between py-3">
       <div class="text-2xl mt-1 flex items-center mx-4">
         <span class="text-gray-700 mr-3 shadow"
@@ -32,7 +21,7 @@
         scrollbar-w-2
         scrolling-touch
         bg-gray-200
-        h-96
+        hoho
       "
     >
       <div v-for="message in messages" :key="message.time" class="chat-message">
@@ -93,71 +82,17 @@
       </div>
     </div>
 
-    <div v-if="messages" class="pt-4 mb-2 sm:mb-0">
-      <div class="flex">
-        <input
-          @keypress.enter="sendMessage(newMessage)"
-          v-model="newMessage"
-          type="text"
-          placeholder="Write Something"
-          class="
-            mx-2
-            w-full
-            focus:outline-none focus:placeholder-gray-400
-            text-gray-600
-            placeholder-gray-600
-            pl-6
-            bg-gray-200
-            rounded-full
-            py-3
-          "
-        />
-
-        <button
-          @click="sendMessage(newMessage)"
-          type="button"
-          class="
-            mx-2
-            inline-flex
-            items-center
-            justify-center
-            rounded-full
-            h-12
-            w-12
-            transition
-            duration-500
-            ease-in-out
-            text-white
-            bg-cyan-500
-            hover:bg-blue-400
-            focus:outline-none
-          "
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="h-6 w-6 transform rotate-90"
-          >
-            <path
-              d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
-            ></path>
-          </svg>
-        </button>
-      </div>
-    </div>
+    <app-message-input v-if="messages" />
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import AppMessageInput from "./AppMessageInput.vue";
 
 export default {
   props: ["messages", "searchedUser"],
-  data() {
-    return {
-      newMessage: "",
-    };
+  components: {
+    AppMessageInput,
   },
   computed: {
     getEmailFromStorage() {
@@ -165,15 +100,6 @@ export default {
     },
   },
   methods: {
-    ...mapActions({
-      send: "chat/sendNewMessage",
-    }),
-    async sendMessage(newMessage) {
-      if (newMessage) {
-        await this.send(newMessage);
-      }
-      this.newMessage = "";
-    },
     updateScroll() {
       let chatScroll = document.getElementById("messages");
       chatScroll.scrollTop = chatScroll.scrollHeight;
@@ -184,18 +110,17 @@ export default {
       return `${formatArray[4]} (${formatArray[1]} ${formatArray[2]})`;
     },
   },
-  watch: {
-    messages() {
-      this.updateScroll();
-    },
-  },
-  mounted() {
+  updated() {
     this.updateScroll();
   },
 };
 </script>
 
 <style>
+.hoho {
+  height: 60vh;
+}
+
 .scrollbar-w-2::-webkit-scrollbar {
   width: 0.25rem;
   height: 0.25rem;
